6284d0fe7ec65a81460eec2655fd42b4407d7792
kstensing <stensingk@gmail.com>
2022-03-01 21:26:47 -0600

    Add  Heroku

--- server.js
+++ server.js
@@ -3,8 +3,43 @@ const { animals } = require('./data/animals.json')
 
 const app = express();
 
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        // Save personalityTraits as a dedicated array.
+    // If personalityTraits is a string, place it into a new array and save.
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        // loop through each trait in the personalityTraits array:
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    } 
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    } 
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
 app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
+    let results = animals;
+    if (req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 });
 
 app.listen(3001, () => {
