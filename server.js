174f61d0e2c9cccefdef3db2137d568922fcb93f
kstensing <stensingk@gmail.com>
2022-03-02 22:58:23 -0600

    Add heroku

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
-const express = require('express');
-const { animals } = require('./data/animals.json')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -107,7 +105,7 @@ app.get('/animals', (req, res) => {
   res.sendFile(path.join(__dirname, './public/animals.html'));
 });
 
-app.get('/zookepers', (req, res) => {
+app.get('/zookeepers', (req, res) => {
   res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
@@ -118,60 +116,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        // Save personalityTraits as a dedicated array.
-    // If personalityTraits is a string, place it into a new array and save.
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        } else {
-            personalityTraitsArray = query.personalityTraits;
-        }
-        // loop through each trait in the personalityTraits array:
-        personalityTraitsArray.forEach(trait => {
-            filteredResults = filteredResults.filter(
-                animal => animal.personalityTraits.indexOf(trait) !== -1
-            );
-        });
-    } 
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    } 
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
-};
-
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-}
-
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query){
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.sendStatus(404);
-    }
-});
-
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
-})
