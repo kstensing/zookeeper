4c97e1166d83acdfdffd83088beb92f94eff8581
kstensing <stensingk@gmail.com>
2022-03-04 20:36:50 -0600

    added zookeeper form

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
-router.use(require('./zookeeperRoutes'));
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
 
 router.use(animalRoutes);
+router.use(zookeeperRoutes);
 
 
 module.exports = router;
